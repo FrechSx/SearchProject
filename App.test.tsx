fb6ab29f9d425e89a77212eab4290041a1b66b69
Frech <136808794+FrechSx@users.noreply.github.com>
2023-06-19 02:35:18 +0700

    Update App.test.tsx

--- App.test.tsx
+++ App.test.tsx
@@ -5,5 +5,7 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+  const linkElement2 = screen.getByText(/learn react/i);
+  console.log(linkElement2);
   expect(linkElement).toBeInTheDocument();
 });
